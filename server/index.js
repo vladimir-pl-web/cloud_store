ffaa3551767702e323965060c633589170723a3c
Vladimir <vladimirplotnikov371@gmail.com>
2023-04-04 23:18:01 +0300

    auth

--- server/index.js
+++ server/index.js
@@ -2,24 +2,26 @@ import express from 'express'
 import mongoose from 'mongoose'
 import config from 'config'
 import authRouter from './routes/User.js'
-const app = express()
+import cors from 'cors'
+import { corsMiddleware } from './middlwares/middlwares.js'
 const PORT = config.get("server")
 const URL = config.get("dbURL")
-
+const app = express()
 
 app.use(express.json())
 app.use('/api/auth', authRouter)
+app.use(cors())
 
-const start = async ()=>{
- try {
-   await mongoose.connect(URL, {
-   
-  })
-  app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
- }
- catch (e) {
-  
- }
+const start = async () => {
+  try {
+    await mongoose.connect(URL, {
+
+    })
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  }
+  catch (e) {
+
+  }
 }
 
 start()
--- server/routes/User.js
+++ server/routes/User.js
@@ -11,66 +11,65 @@ const router = new Router()
 
 router.post('/registration', [
  check('email', "Wrong email").isEmail(),
- check('password', 'Password must be longer than 3').isLength({min:3})
- 
-],async (req, res) => {
+ check('password', 'Password must at least 5 symbols').isLength({ min: 5 })
+
+], async (req, res) => {
  try {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-   return res.status(400).json({message: "Uncorrect request", errors})
+   return res.status(400).json({ message: "Uncorrect request", errors })
   }
   const { email, password } = req.body
-  
+
   const candidate = await User.findOne({ email })
-  
+
   if (candidate) {
-   return res.status(400).json({message: `User with email ${email} already exist`})
+   return res.status(400).json({ message: `User with email ${email} already exist` })
   }
 
   const hashedPassword = await bcrypt.hash(password, 15)
-  const user = new User({email, password: hashedPassword})
+  const user = new User({ email, password: hashedPassword })
   await user.save()
-  return res.json({message: "User created"})
+  return res.json({ message: "User created" })
  }
  catch (e) {
-  console.log(e)
-  res.send({message: "server error"})
+  return res.json({ message: e.res.data.message })
  }
 })
 
 router.post('/login', [
 
-],async (req, res) => {
+], async (req, res) => {
  try {
-  
-  const {email, password} = req.body
-  const user = await User.findOne({email })
 
-  
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+
   if (!user) {
-   return res.status(404).json({message:"User not found"})
+   return res.status(404).json({ message: "User not found" })
   }
 
   const isPassValid = bcrypt.compareSync(password, user.password)
   if (!isPassValid) {
-   return res.status(400).json({message:"Invalid password"})
+   return res.status(400).json({ message: "Invalid password" })
   }
 
   const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: EXPIRES })
-  const {diskSpace, id, usedSpace, files} = user
+  const { diskSpace, id, usedSpace, files } = user
 
   return res.json({
    message: "Logged in",
    token,
    expiresIn: new Date(new Date().setDate(new Date().getDate() + +EXPIRES[0])),
-   user_data:{email, diskSpace, id, usedSpace, files}
+   user_data: { email, diskSpace, id, usedSpace, files }
   })
 
  }
  catch (e) {
   console.log(e)
-  res.send({message: "server error"})
+  res.send({ message: "server error" })
  }
 })
 
